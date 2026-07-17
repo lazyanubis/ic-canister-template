fb808860bb4fb9bf500d34e5d7b8d85c93485876
Anubis <46840787+lazyanubis@users.noreply.github.com>
2026-07-17 17:00:49 +0800

    Update business.rs

--- src/stable/v001/business.rs
+++ src/stable/v001/business.rs
@@ -5,7 +5,6 @@ impl Business for InnerState {
     fn business_example_query(&self) -> String {
         self.example_data.clone()
     }
-
     fn business_example_count_query(&self) -> u64 {
         self.example_count
     }
